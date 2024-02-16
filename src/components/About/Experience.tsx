05ea3c73f73c21fe89eaaa4ea26630e29a0cd8f0
Lucas <lcipriane@mcfadyen.com>
2024-02-16 14:09:39 -0300

    Restart project with App router

--- src/components/About/Experience.tsx
+++
@@ -1,7 +0,0 @@
-//import classes from './Experience.module.scss';
-
-const Experience = () => {
-  return <section></section>;
-};
-
-export default Experience;
--- src/pages/contact.tsx
+++
@@ -1,9 +0,0 @@
-const ContactPage = () => {
-  return (
-    <main>
-      <h1>Contact Me</h1>
-    </main>
-  );
-};
-
-export default ContactPage;
