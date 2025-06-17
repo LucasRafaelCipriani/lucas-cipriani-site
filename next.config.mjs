b7aab8dc21c5f8af66eff2d2837dfe05e7b689a1
Lucas Cipriani <lcipriane@mcfadyen.com>
2025-06-17 19:38:31 -0300

    Deployment fixes

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
 
-    config.resolve.alias['@'] = path.join(__dirname);
+    config.resolve.alias['@'] = path.resolve(__dirname);
 
     return config;
   },
