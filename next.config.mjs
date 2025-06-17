5d605cc11ac5d828bddefb4e79ec9dae4327f4e8
Lucas Cipriani <lcipriane@mcfadyen.com>
2025-06-17 19:29:50 -0300

    Deployment fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     unoptimized: true,
+  },
+  webpack: (config) => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
   },
 };
 
